a5d5dc6d0c58e10f318d2baa1d2a17cc1d53b72d
thiagorjes <thiagorjes@outlook.com>
2019-10-12 21:55:13 -0300

    novos scripts

---
+++ scripts/plot_yolo_loss_new.py
@@ -0,0 +1,32 @@
+import argparse 
+import sys
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import time
+
+def main(i):
+
+    f = open("/tmp/train.log")
+
+    lines  = [line.rstrip("\n") for line in f.readlines()]
+    numbers = {'1','2','3','4','5','6','7','8','9'}
+
+    iters = []
+    loss = []
+    prev_line = ""
+    for line in lines:
+        args = line.split(' ')
+        if args[0][-1:]==':' and args[0][0] in numbers :
+            iters.append(int(args[0][:-1]))            
+            loss.append(float(args[2]))
+    ax.clear()
+    ax.plot(iters,loss)
+    plt.xlabel('iters')
+    plt.ylabel('loss')
+    plt.grid()
+
+fig,ax = plt.subplots()
+
+if __name__ == "__main__":
+    ani = animation.FuncAnimation(fig,main,interval=1000)
+    plt.show()
